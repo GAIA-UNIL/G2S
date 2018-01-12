2a6d1af0b864f1d3b247dfd621e9b0b50b7bc20c
Mathieu Gravey <gravey.mathieu@gmail.com>
2018-01-12 17:00:39 +0100

    fix issue with python compiling

--- build/python-build/compile.py
+++ build/python-build/compile.py
@@ -11,7 +11,7 @@ setup(
     	language="c++", 
     	extra_compile_args=["-std=c++11"],
     	extra_link_args=["-std=c++11"],
-    	include_dirs=["../../include", "/usr/include","/opt/local/include"],
+    	include_dirs=["../../include", "/usr/include","/usr/include/jsoncpp","/opt/local/include"],
     	libraries = ['zmq','jsoncpp'],
         library_dirs = ['/usr/lib','/opt/local/lib']
          )],
