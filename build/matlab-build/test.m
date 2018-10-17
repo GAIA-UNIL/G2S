b59112c4c5922537cbd30832d34a476a8106cc87
Mathieu Gravey <gravey.mathieu@gmail.com>
2018-10-17 21:12:00 +0200

    Improuve files and some test in Matlab test file

--- build/matlab-build/test.m
+++ build/matlab-build/test.m
@@ -25,22 +25,22 @@ sourceCat=single(sourceCat);
 
 %% simple echo
 
-data=g2s('-sa',serverAddress,'-a','echo','-ti',single(source),'-dt',zeros(1,1));
+data=g2s('-sa',serverAddress,'-a','echo','-ti',source,'-dt',zeros(1,1));
 imshow(data);
 
 
 %% simple unconditional simulation with QS
 
-[data,t]=g2s('-sa',serverAddress,'-a','qs','-ti',single(source),'-di',destination,'-dt',zeros(1,1),'-k',1.5,'-n',50,'-s',100);
+[data,t]=g2s('-sa',serverAddress,'-a','qs','-ti',source,'-di',destination,'-dt',zeros(1,1),'-k',1.5,'-n',50,'-s',100);
 imshow(data);
 disp(t)
 
 %% simple unconditional simulation with QS with GPU if integrated GPU avaible
-data=g2s('-sa',serverAddress,'-a','qs','-ti',single(source),'-di',destination,'-dt',zeros(1,1),'-k',1.5,'-n',50,'-s',100,'-W_GPU');
+data=g2s('-sa',serverAddress,'-a','qs','-ti',source,'-di',destination,'-dt',zeros(1,1),'-k',1.5,'-n',50,'-s',100,'-W_GPU');
 imshow(data);
 
 %% simulation with random value at random position
-data=g2s('-sa',serverAddress,'-a','qs','-ti',single(source),'-di',single(conDestination),'-dt',zeros(1,1),'-k',1.5,'-n',50,'-s',100);
+data=g2s('-sa',serverAddress,'-a','qs','-ti',source,'-di',single(conDestination),'-dt',zeros(1,1),'-k',1.5,'-n',50,'-s',100);
 imshow(data);
 
 %% simple non conditional simulation with multi TI of differente size
@@ -51,14 +51,14 @@ imshow(data);
 %% simulation with a fixed path, row path
 path=zeros(200);
 path(:)=(1:200*200);
-data=g2s('-sa',serverAddress,'-a','qs','-ti',single(source),'-di',destination,'-dt',zeros(1,1),'-k',1.5,'-n',50,'-s',100,'-sp',path);
+data=g2s('-sa',serverAddress,'-a','qs','-ti',source,'-di',destination,'-dt',zeros(1,1),'-k',1.5,'-n',50,'-s',100,'-sp',path);
 imshow(data);
 
 %% simulation with a fixed path, partial random path
 path=zeros(200);
 path(:)=randperm(200*200);
 path(randperm(200*200,200*200/2))=-inf;
-data=g2s('-sa',serverAddress,'-a','qs','-ti',single(source),'-di',destination,'-dt',zeros(1,1),'-k',1.5,'-n',50,'-s',100,'-sp',path);
+data=g2s('-sa',serverAddress,'-a','qs','-ti',source,'-di',destination,'-dt',zeros(1,1),'-k',1.5,'-n',50,'-s',100,'-sp',path);
 imshow(data);
 
 %% specifing a kernel
@@ -68,23 +68,35 @@ kernel=zeros(101,101);
 kernel(51,51)=1;
 kernel=exp(-0.1*bwdist(kernel));
 
-data=g2s('-sa',serverAddress,'-a','qs','-ti',single(source),'-di',destination,'-dt',zeros(1,1),'-k',1.5,'-n',50,'-s',100,'-ki',kernel);
+data=g2s('-sa',serverAddress,'-a','qs','-ti',source,'-di',destination,'-dt',zeros(1,1),'-k',1.5,'-n',50,'-s',100,'-ki',kernel);
 imshow(data);
 
 %% Multivariate
-
 source3=cat(3,source,source,source);
 destination3=cat(3,destination,destination,destination);
-data=g2s('-sa',serverAddress,'-a','qs','-ti',single(source3),'-di',destination3,'-dt',zeros(1,3),'-k',1.5,'-n',50,'-s',100);
+data=g2s('-sa',serverAddress,'-a','qs','-ti',source3,'-di',destination3,'-dt',zeros(1,3),'-k',1.5,'-n',50,'-s',100);
 imshow(data);
 
 %% Multi-threaded, if supported
 nbThreads=4;
-data=g2s('-sa',serverAddress,'-a','qs','-ti',single(source),'-di',destination,'-dt',zeros(1,1),'-k',1.5,'-n',50,'-s',100,'-j',nbThreads);
+data=g2s('-sa',serverAddress,'-a','qs','-ti',source,'-di',destination,'-dt',zeros(1,1),'-k',1.5,'-n',50,'-s',100,'-j',nbThreads);
+imshow(data);
+
+%% With missing data
+incompleteSource=source;
+incompleteSource(rand(size(incompleteSource))>0.90)=nan; % remove randomly 10% of the data
+data=g2s('-sa',serverAddress,'-a','qs','-ti',incompleteSource,'-di',destination,'-dt',zeros(1,1),'-k',1.5,'-n',50,'-s',100);
+imshow(data);
+
+%% Multivariate with missing data
+incompleteSource3=cat(3,source,source,source);
+destination3=cat(3,destination,destination,destination);
+incompleteSource3(rand(size(incompleteSource3))>0.90)=nan; % remove randomly 10% of the data
+data=g2s('-sa',serverAddress,'-a','qs','-ti',incompleteSource3,'-di',destination3,'-dt',zeros(1,3),'-k',1.5,'-n',50,'-s',100);
 imshow(data);
 
 %% DS mode
-data=g2s('-sa',serverAddress,'-a','ds-l','-ti',single(source),'-di',destination,'-dt',zeros(1,1),'-th',0.05,'-f',0.3,'-n',50,'-s',100);
+data=g2s('-sa',serverAddress,'-a','ds-l','-ti',source,'-di',destination,'-dt',zeros(1,1),'-th',0.05,'-f',0.3,'-n',50,'-s',100);
 imshow(data);
 
 %% Categorical Mode
