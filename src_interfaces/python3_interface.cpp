47d379eb39051ab5fc2ae8f1735c930678f0ea8b
Mathieu Gravey <mathieu.gravey@unil.ch>
2019-06-05 11:04:14 +0200

    update python module to include posible python extenstion on the server side

--- src_interfaces/python3_interface.cpp
+++ src_interfaces/python3_interface.cpp
@@ -5,11 +5,17 @@ typedef unsigned jobIdType;
 
 static char module_docstring[] =
 "This module provides an interface for computing geostatistical simulation remotly using G2S";
-static char run_docstring[] =
-"Executing a simulation";
+static char run_docstring[] = "Executing a simulation";
+static char load_docstring[] = "load a dataImage from G2S";
+static char write_docstring[] = "write a dataImage from G2S";
 static PyObject *g2s_run(PyObject *self, PyObject *args, PyObject *keywds);
+static PyObject *g2s_loadData(PyObject *self, PyObject *args, PyObject *keywds);
+static PyObject *g2s_writeData(PyObject *self, PyObject *args, PyObject *keywds);
+
 static PyMethodDef module_methods[] = {
 	{"run", (PyCFunction)g2s_run, METH_VARARGS|METH_KEYWORDS, run_docstring},
+	{"loadData", (PyCFunction)g2s_loadData, METH_VARARGS|METH_KEYWORDS, load_docstring},
+	{"writeData", (PyCFunction)g2s_writeData, METH_VARARGS|METH_KEYWORDS, write_docstring},
 	{NULL, NULL, 0, NULL}
 };
 
@@ -75,5 +81,60 @@ static PyObject *g2s_run(PyObject *self, PyObject *args, PyObject *keywds)
 {
 	InerfaceTemplatePython3 inerfaceTemplatePython3;
 	return inerfaceTemplatePython3.runStandardCommunicationPython(self, args, keywds, pythonNumberOfOutputParameter() );
+
+}
+
+static PyObject *g2s_loadData(PyObject *self, PyObject *args, PyObject *keywds)
+{
+#ifndef _WIN32
+	static char *kwlist[] = {"filename", NULL};
+	PyObject* filename;
+	if (!PyArg_ParseTupleAndKeywords(args, keywds, "U", kwlist, &filename)) {
+        return NULL;
+    }
+	InerfaceTemplatePython3 inerfaceTemplatePython3;
+	std::string filenameStr=inerfaceTemplatePython3.nativeToStandardString(std::any(filename));
+	g2s::DataImage loadedImage=g2s::DataImage::createFromFile(filenameStr);
+	PyObject* imageNumpy=std::any_cast<PyObject*>(inerfaceTemplatePython3.convert2NativeMatrix(loadedImage));
+	long size=(loadedImage._types.size());
+	PyObject* type=PyArray_SimpleNew(1,&size,NPY_INT32);
+	int32_t *typePtr=(int32_t*)PyArray_DATA(type);
+
+	for (int i = 0; i < loadedImage._types.size(); ++i)
+	{
+		switch(loadedImage._types[i]){
+			case g2s::DataImage::VaraibleType::Continuous:
+			typePtr[i]=0;
+			break;
+			case g2s::DataImage::VaraibleType::Categorical:
+			typePtr[i]=1;
+			break;
+		}
+	}
+	PyObject* pyResult=PyTuple_New(2);
+	PyTuple_SetItem(pyResult,0,imageNumpy);
+	PyTuple_SetItem(pyResult,1,type);
+	return pyResult;
+#else
+	Py_RETURN_NONE;
+#endif
+}
+
+static PyObject *g2s_writeData(PyObject *self, PyObject *args, PyObject *keywds)
+{
+#ifndef _WIN32
+	static char *kwlist[] = {"image","dataType","filename", NULL};
+	PyObject* pyImage;
+	PyObject* pyDataType;
+	PyObject* filename;
+	if (!PyArg_ParseTupleAndKeywords(args, keywds, "OOU", kwlist, &pyImage, &pyDataType, &filename)) {
+        return NULL;
+    }
+	InerfaceTemplatePython3 inerfaceTemplatePython3;
+	g2s::DataImage image=inerfaceTemplatePython3.convertNativeMatrix2DataImage(std::any(pyImage),std::any(pyDataType));
+	std::string filenameStr=inerfaceTemplatePython3.nativeToStandardString(std::any(filename));
+	image.write(filenameStr);
+#endif
+	Py_RETURN_NONE;
 
 }
