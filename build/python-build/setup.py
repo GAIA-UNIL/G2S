dac7df372517a1bcb536b51735b2ce310ca7974b
Mathieu Gravey <mathieu.gravey@unil.ch>
2019-11-25 16:26:02 +0100

    dev26

--- build/python-build/setup.py
+++ build/python-build/setup.py
@@ -44,7 +44,7 @@ if(systemName=='Darwin' or systemName=='Linux'):
 		ext_package = 'g2s',
 		ext_modules=[Extension("g2s", sources=["../../src_interfaces/python3_interface.cpp"],
 			language="c++", 
-			extra_compile_args=["-std=c++17",'-DVERSION='+extra+'\"'+open('../../version', 'r').read()+extra+'\"','-DPYTHON_VERSION=\"'+platform.python_version()+'\"'],
+			extra_compile_args=["-std=c++17",'-DVERSION='+extra+'\"'+open('../../version', 'r').read()+extra+'\"','-DPYTHON_VERSION='+extra+'\"'+platform.python_version()+extra+'\"'],
 			extra_link_args=["-std=c++17"],
 			include_dirs=["../../include","../../include_interfaces", "/usr/include","/usr/include/jsoncpp","/opt/local/include"],
 			libraries = ['zmq','jsoncpp'],
