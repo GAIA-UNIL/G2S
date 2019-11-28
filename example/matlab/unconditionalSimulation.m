857429c5df361fa826e1c9a56ff0d6205f2470cd
Mathieu Gravey <mathieu.gravey@unil.ch>
2019-11-28 12:46:37 +0100

    add example for (un)conditional simulations and gap filling

--- example/matlab/unconditionalSimulation.m
+++ example/matlab/unconditionalSimulation.m
@@ -1,1 +1,15 @@
-% coming soon
+%load data
+ti=imread('https://raw.githubusercontent.com/GAIA-UNIL/TrainingImagesTIFF/master/stone.tiff');
+
+% QS call using G2S
+simulation=g2s('-a','qs','-ti',ti,'-di',nan(200,200),'-dt',[0],'-k',1.2,'-n',50,'-j',0.5);
+
+%Display results 
+suptitle('Unconditional simulation');
+subplot(1,2,1);
+imshow(ti);
+title('Training image');
+subplot(1,2,2);
+imshow(simulation);
+title('Simulation');
+
--- example/python/gapFilling.py
+++ example/python/gapFilling.py
@@ -1,1 +1,24 @@
-# coming soon
+import numpy
+from PIL import Image
+import requests
+from io import BytesIO
+from g2s import g2s
+import matplotlib.pyplot as plt
+
+#load data
+tiWithGap = numpy.array(Image.open(BytesIO(requests.get('https://raw.githubusercontent.com/GAIA-UNIL/TrainingImagesTIFF/master/stone.tiff').content)));
+tiWithGap[60:140,60:140]=numpy.nan;
+
+# QS call using G2S
+simulation,_=g2s('-a','qs','-ti',tiWithGap,'-di',tiWithGap,'-dt',numpy.zeros((1,)),'-k',1.2,'-n',25,'-j',0.5);
+
+#Display results 
+fig, (ax1, ax2) = plt.subplots(1, 2)
+fig.suptitle('Gap filling')
+ax1.imshow(tiWithGap)
+ax1.set_title('Training image');
+ax1.axis('off');
+ax2.imshow(simulation)
+ax2.set_title('Simulation');
+ax2.axis('off');
+plt.show()
