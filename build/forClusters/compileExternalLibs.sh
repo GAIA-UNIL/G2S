72e43756f2305f538d6d9f6c58d0377807e6f5cb
Mathieu Gravey <mathieu.gravey@unil.ch>
2020-03-21 12:23:14 +0100

    Update compileExternalLibs.sh

--- build/forClusters/compileExternalLibs.sh
+++ build/forClusters/compileExternalLibs.sh
@@ -7,21 +7,30 @@ dir=$(pwd)/selfCompiled;
 mkdir -p $dir
 
 if [[ ! -d libzmq ]]; then
-	git clone https://github.com/zeromq/libzmq.git
+      git clone https://github.com/zeromq/libzmq.git
 fi
 cd libzmq
 git pull
 git clean -f
-./autogen.sh
-./configure --prefix=$dir
-make -j
-make install
+if [[ $(command -v autoconf) ]]; then
+      bash ./autogen.sh
+      ./configure --prefix=$dir
+      make -j
+      make install
+else
+      mkdir build
+      cd build
+      cmake -DCMAKE_INSTALL_PREFIX=$dir  ..
+      make -j
+      make install
+      cd ..
+fi
 cd ..
 
 wget "https://raw.githubusercontent.com/zeromq/cppzmq/5999e5adc7eeca10bb469246ee3909da037c9036/zmq.hpp" -O ${dir}/include/zmq.hpp
 
 if [[ ! -d jsoncpp ]]; then
-	git clone https://github.com/open-source-parsers/jsoncpp.git
+      git clone https://github.com/open-source-parsers/jsoncpp.git
 fi
 cd jsoncpp
 git pull
@@ -35,15 +44,15 @@ make install
 cd ../../..
 
 if [[ ! -d fftw-3.3.8 ]]; then
-	wget "http://www.fftw.org/fftw-3.3.8.tar.gz"
-	tar -xf fftw-3.3.8.tar.gz
+      wget "http://www.fftw.org/fftw-3.3.8.tar.gz"
+      tar -xf fftw-3.3.8.tar.gz
 fi
 cd fftw-3.3.8
 make clean
 
 dosentSupportOMP= $(shell sh -c 'echo "int main(){}" | c++ -xc - -o /dev/null -fopenmp 2>/dev/null && echo 0 || echo 1 ')
 if [[ dosentSupportOMP ]]; then
-	./configure --prefix=$dir    \
+      ./configure --prefix=$dir    \
             --enable-shared  \
             --enable-threads \
             --enable-sse2    \
@@ -53,7 +62,7 @@ if [[ dosentSupportOMP ]]; then
             --enable-threads \
             # --enable-openmp
 else
-        	./configure --prefix=$dir    \
+            ./configure --prefix=$dir    \
             --enable-shared  \
             --enable-threads \
             --enable-sse2    \
@@ -73,9 +82,9 @@ cd ../build
 
 # git checkout Makefile
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	sed -i '' "/export LIBINC=/s/$/ -I..\/..\/external\/selfCompiled\/include/" Makefile
-	sed -i '' "/export LIB_PATH=/s/$/ -L..\/..\/external\/selfCompiled\/lib/" Makefile
+      sed -i '' "/export LIBINC=/s/$/ -I..\/..\/external\/selfCompiled\/include/" Makefile
+      sed -i '' "/export LIB_PATH=/s/$/ -L..\/..\/external\/selfCompiled\/lib/" Makefile
 else
-	sed -i "/export LIBINC=/s/$/ -I..\/..\/external\/selfCompiled\/include/" Makefile
-	sed -i "/export LIB_PATH=/s/$/ -L..\/..\/external\/selfCompiled\/lib/" Makefile
+      sed -i "/export LIBINC=/s/$/ -I..\/..\/external\/selfCompiled\/include/" Makefile
+      sed -i "/export LIB_PATH=/s/$/ -L..\/..\/external\/selfCompiled\/lib/" Makefile
 fi
