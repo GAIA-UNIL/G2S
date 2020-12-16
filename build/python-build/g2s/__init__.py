1e445180c12b95cc6e1d51559b960e0ac2777da9
Mathieu Gravey <mathieu.gravey@unil.ch>
2020-12-16 14:07:23 +0100

    improuve windows integration of python interface

--- build/python-build/g2s/__init__.py
+++ build/python-build/g2s/__init__.py
@@ -1,2 +1,13 @@
+import os
+import sys
+
+if sys.platform == 'win32' and os.path.isdir(extra_dll_dir):
+	extra_dll_dir = os.path.join(os.path.dirname(__file__), '.libs')
+	if sys.version_info >= (3, 8):
+		os.add_dll_directory(extra_dll_dir)
+	else:
+		os.environ.setdefault('PATH', '')
+		os.environ['PATH'] += os.pathsep + extra_dll_dir
+
 from .g2s import run
 from .g2s import run as g2s
