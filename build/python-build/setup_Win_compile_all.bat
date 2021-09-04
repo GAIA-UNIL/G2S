98faf400a9dc216b5eb936683bdc156c0094b11e
Mathieu Gravey <mathieu@mgravey.com>
2021-09-04 21:56:50 +0200

    Update setup_Win_compile_all.bat

--- build/python-build/setup_Win_compile_all.bat
+++ build/python-build/setup_Win_compile_all.bat
@@ -1,12 +1,3 @@
-if NOT exist "libzmq" (
-	git clone https://github.com/zeromq/libzmq
-	cd libzmq
-	mkdir action_build
-	cd action_build
-	cmake ..
-	msbuild ZeroMQ.sln /property:Configuration=Release /m:4
-)
-
 if NOT exist "cppzmq-master" (
 	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://codeload.github.com/zeromq/cppzmq/zip/master', 'cppzmq-master.zip')"
 	powershell Expand-Archive cppzmq-master.zip -DestinationPath .
@@ -19,3 +10,14 @@ if NOT exist "jsoncpp-master" (
 	py amalgamate.py
 	cd ..
 )
+
+if NOT exist "libzmq" (
+	git clone https://github.com/zeromq/libzmq
+	cd libzmq
+	mkdir action_build
+	cd action_build
+	cmake ..
+	msbuild ZeroMQ.sln /property:Configuration=Release /m:4
+	cd ..
+)
+
