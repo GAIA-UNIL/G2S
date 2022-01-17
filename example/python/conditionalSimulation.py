2ff4f2b9234c2a0155401b16b0b5b7ee60a69bab
pauwiersma <pauwiersma@outlook.com>
2022-01-17 15:28:47 +0100

    Suptitle

--- example/python/conditionalSimulation.py
+++ example/python/conditionalSimulation.py
@@ -7,20 +7,27 @@ from io import BytesIO
 from g2s import g2s
 import matplotlib.pyplot as plt
 
-# load data
-ti = numpy.array(Image.open(BytesIO(requests.get('https://raw.githubusercontent.com/GAIA-UNIL/TrainingImagesTIFF/master/stone.tiff').content)));
+# load example training image ('stone')
+ti = numpy.array(Image.open(BytesIO(requests.get(
+    'https://raw.githubusercontent.com/GAIA-UNIL/TrainingImagesTIFF/master/stone.tiff').content)));
 
-# empty grid 
-conditioning=numpy.zeros((200,200))*numpy.nan; 
+# create empty grid and add conditioning data
+conditioning = numpy.zeros((200,200))*numpy.nan; 
 # fill the grid with 50 random points
 conditioning.flat[numpy.random.permutation(conditioning.size)[:50]]=ti.flat[numpy.random.permutation(ti.size)[:50]];
 
 # QS call using G2S
-simulation,_=g2s('-a','qs','-ti',ti,'-di',conditioning,'-dt',numpy.zeros((1,)),'-k',1.2,'-n',50,'-j',0.5);
+simulation,_=g2s('-a','qs', 
+                 '-ti',ti,
+                 '-di',conditioning,
+                 '-dt',numpy.zeros((1,)),
+                 '-k',1.2,
+                 '-n',30,
+                 '-j',0.5);
 
 # Display results 
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
-fig.suptitle('Unconditional simulation')
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3,figsize=(7,4))
+fig.suptitle('QS Conditional simulation',size='xx-large',y=0.9)
 ax1.imshow(ti)
 ax1.set_title('Training image');
 ax1.axis('off');
