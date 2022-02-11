82ead883cc2440b74bec3bf4b064c67fb40be9ce
pau_wiersma <pauwiersma@outlook.com>
2022-02-11 06:27:21 -0800

    Add 3d and downscaling examples

---
+++ example/python/3D.py
@@ -0,0 +1,39 @@
+import numpy as np
+import requests
+from io import BytesIO
+from g2s import g2s
+import matplotlib.pyplot as plt
+import tifffile as tf
+from matplotlib import cm
+
+#Load example boolean training image "Damier 3D" and crop it to reduce computational requirements
+dim = 30
+ti = tf.imread(BytesIO(requests.get(
+    'https://raw.githubusercontent.com/GAIA-UNIL/TrainingImagesTIFF/master/Damier3D.tiff').content))[:dim,:dim,:dim]
+
+# QS call using G2S
+simulation,index,time,_=g2s('-a','qs',
+                             '-ti',ti,
+                             '-di',np.zeros_like(ti)*np.nan,
+                             '-dt',[1], #1 for categorical variables
+                             '-k',1.2,
+                             '-n',30)
+
+#Display results
+fig = plt.figure(figsize=(20,15))
+ax1=fig.add_subplot(121,projection='3d')
+ax1.voxels(ti,alpha=0.9,facecolor='tab:blue',edgecolor='black')
+ax1.view_init(azim=45)
+ax1.set_title('3D Training image')
+ax2=fig.add_subplot(122,projection='3d')
+ax2.voxels(simulation,alpha=0.9,facecolor='tab:blue',edgecolor='black')
+ax2.view_init(azim=45)
+ax2.set_title('QS 3D simulation')
+
+#Display the indices of the ti-values found in the di-image
+viridis = cm.get_cmap('viridis',index.max())
+colormap = viridis(index)
+fig = plt.figure(figsize=(10,10))
+ax1=fig.add_subplot(111,projection='3d')
+ax1.voxels(index,facecolors=colormap)
+ax1.view_init(azim=45);
---
+++ example/python/downscaling.py
@@ -0,0 +1,58 @@
+import numpy as np
+from PIL import Image
+from g2s import g2s
+import matplotlib.pyplot as plt
+from matplotlib import colors
+from io import BytesIO
+import requests
+
+#Load example training image "Dunes gobi"
+ti_full = Image.open(BytesIO(requests.get(
+    'https://raw.githubusercontent.com/GAIA-UNIL/TrainingImagesTIFF/master/dunes_gobi.tiff').content))
+#Keep one version at fine resolution
+ti_fine = np.array(ti_full)
+#Make a synthetic coarse resolution image by upscaling and reprojecting to the original grid 
+ti_coarse = np.array(ti_full.resize(
+    (int(ti_full.width/5),int(ti_full.height/5))).resize(
+    (ti_full.width,ti_full.height),resample=Image.NEAREST))
+
+#Display the full training image at both resolutions
+norm =colors.Normalize(vmin=ti_fine.min(),vmax=ti_fine.max())
+f,(ax1,ax2) = plt.subplots(2,1,figsize=(12,12))
+ax1.imshow(ti_fine,norm=norm)
+ax1.set_title('Fine resolution training image')
+ax2.imshow(ti_coarse, norm =norm)
+ax2.set_title('Coarse resolution training image')
+
+#Crop half of the image to be used as ti
+ti = np.stack((ti_fine[:500,:500],ti_coarse[:500,:500]),axis=2)
+#Crop upper right corner to be used as di
+di_coarse = ti_coarse[:200,-200:]
+di_fine   = np.zeros((200,200))*np.nan
+di=np.stack((di_fine,di_coarse),axis=2)
+#dt consists of two zeros representing two continuous variables
+dt = [0]*ti.shape[-1]
+
+# QS call using G2S
+simulation,index,_=g2s('-a','qs',
+                         '-ti',ti,
+                         '-di',di,
+                         '-dt',dt,
+                         '-k',1.2,
+                         '-n',50,
+                         '-j',0.5)
+
+#Display results 
+norm =colors.Normalize(vmin=ti_fine.min(),vmax=ti_fine.max())
+f,((ax1,ax2),(ax3,ax4)) = plt.subplots(2,2,figsize=(12,12),sharex=True,sharey=True)
+plt.subplots_adjust(wspace=0.1,hspace=0.1)
+plt.suptitle('QS Downscaling',size='xx-large')
+ax1.imshow(di_coarse,norm=norm)
+ax1.set_title('Coarse res di')
+ax2.imshow(simulation[:,:,0], norm =norm)
+ax2.set_title('Simulation')
+ax3.imshow(index)
+ax3.set_title('Index')
+ax4.imshow(ti_fine[:200,-200:],norm=norm)
+ax4.set_title('True image')
+ax4.set_xticks(np.arange(0,200,25),np.arange(ti_full.width-200,ti_full.width,25));
