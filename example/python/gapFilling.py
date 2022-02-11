cb62e83ddb989946c3ed9da1744a83abf5d1c270
pau_wiersma <pauwiersma@outlook.com>
2022-02-11 06:42:28 -0800

    Update python gapfilling

--- example/python/gapFilling.py
+++ example/python/gapFilling.py
@@ -5,16 +5,23 @@ from io import BytesIO
 from g2s import g2s
 import matplotlib.pyplot as plt
 
-#load data
-tiWithGap = numpy.array(Image.open(BytesIO(requests.get('https://raw.githubusercontent.com/GAIA-UNIL/TrainingImagesTIFF/master/stone.tiff').content)));
+# load example training image ('stone') and cut out a part of it
+tiWithGap = numpy.array(Image.open(BytesIO(requests.get(
+    'https://raw.githubusercontent.com/GAIA-UNIL/TrainingImagesTIFF/master/stone.tiff').content)));
 tiWithGap[60:140,60:140]=numpy.nan;
 
 # QS call using G2S
-simulation,_=g2s('-a','qs','-ti',tiWithGap,'-di',tiWithGap,'-dt',numpy.zeros((1,)),'-k',1.2,'-n',25,'-j',0.5);
+simulation,_=g2s('-a','qs',
+                 '-ti',tiWithGap,
+                 '-di',tiWithGap,
+                 '-dt',[0],
+                 '-k',1.2,
+                 '-n',25,
+                 '-j',0.5);
 
 #Display results 
-fig, (ax1, ax2) = plt.subplots(1, 2)
-fig.suptitle('Gap filling')
+fig, (ax1, ax2) = plt.subplots(1, 2,figsize=(7,4))
+fig.suptitle('QS Gap filling',size='xx-large')
 ax1.imshow(tiWithGap)
 ax1.set_title('Training image');
 ax1.axis('off');
