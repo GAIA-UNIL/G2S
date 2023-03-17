4b790c5436705edf25410175d9ebb3a165164a29
Mathieu Gravey <mathieu@mgravey.com>
2023-03-17 20:12:38 +0100

    fix makedeb

--- build/makeDeb.sh
+++ build/makeDeb.sh
@@ -1,14 +1,14 @@
 #!/bin/env basj^h
 make intel -j
-cp ./g2s_algosName.config ./g2s-package/usr/bin/g2s_bin
+cp ./algosName.config ./g2s-package/usr/bin/g2s_bin
 cp ./intel-build/g2s_server ./g2s-package/usr/bin/g2s_bin
-cp ./intel-build/g2s_echo ./g2s-package/usr/bin/g2s_bin
-cp ./intel-build/g2s_qs ./g2s-package/usr/bin/g2s_bin
-cp ./intel-build/g2s_nds ./g2s-package/usr/bin/g2s_bin
-cp ./intel-build/g2s_ds-l ./g2s-package/usr/bin/g2s_bin
-cp ./intel-build/g2s_algoNames ./g2s-package/usr/bin/g2s_bin
-cp ./intel-build/g2s_errorTest ./g2s-package/usr/bin/g2s_bin
-cp ./intel-build/g2s_auto_qs ./g2s-package/usr/bin/g2s_bin
+cp ./intel-build/echo ./g2s-package/usr/bin/g2s_bin
+cp ./intel-build/qs ./g2s-package/usr/bin/g2s_bin
+cp ./intel-build/nds ./g2s-package/usr/bin/g2s_bin
+cp ./intel-build/ds-l ./g2s-package/usr/bin/g2s_bin
+cp ./intel-build/algoNames ./g2s-package/usr/bin/g2s_bin
+cp ./intel-build/errorTest ./g2s-package/usr/bin/g2s_bin
+cp ./intel-build/auto_qs ./g2s-package/usr/bin/g2s_bin
 
 sudo chown root:root -R ./g2s-package/*
 dpkg-deb --build ./g2s-package
