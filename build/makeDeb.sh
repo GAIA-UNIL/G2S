2c5ca485428137979e27f74d1c4d3583d0983176
Mathieu Gravey <mathieu@mgravey.com>
2023-03-18 21:02:51 +0100

    some fix debian

--- build/makeDeb.sh
+++ build/makeDeb.sh
@@ -1,14 +1,15 @@
-#!/bin/env bash
+#!/bin/bash
 make intel -j
 mkdir -p ./g2s-package/usr/bin/g2s_bin/
 cp ./algosName.config ./g2s-package/usr/bin/g2s_bin/
-cp ./intel-build/g2s_server ./g2s-package/usr/bin/g2s_bin/
-cp ./intel-build/echo ./g2s-package/usr/bin/g2s_bin/
-cp ./intel-build/qs ./g2s-package/usr/bin/g2s_bin/
-cp ./intel-build/nds ./g2s-package/usr/bin/g2s_bin/
-cp ./intel-build/ds-l ./g2s-package/usr/bin/g2s_bin/
-cp ./intel-build/errorTest ./g2s-package/usr/bin/g2s_bin/
-cp ./intel-build/auto_qs ./g2s-package/usr/bin/g2s_bin/
+objcopy --strip-debug --strip-unneeded ./intel-build/g2s_server  ./g2s-package/usr/bin/g2s_bin/g2s_server 
+objcopy --strip-debug --strip-unneeded ./intel-build/echo  ./g2s-package/usr/bin/g2s_bin/echo 
+objcopy --strip-debug --strip-unneeded ./intel-build/qs  ./g2s-package/usr/bin/g2s_bin/qs 
+objcopy --strip-debug --strip-unneeded ./intel-build/nds  ./g2s-package/usr/bin/g2s_bin/nds 
+objcopy --strip-debug --strip-unneeded ./intel-build/ds-l ./g2s-package/usr/bin/g2s_bin/ds-l 
+objcopy --strip-debug --strip-unneeded ./intel-build/errorTest  ./g2s-package/usr/bin/g2s_bin/errorTest 
+objcopy --strip-debug --strip-unneeded ./intel-build/auto_qs  ./g2s-package/usr/bin/g2s_bin/auto_qs 
+
 
 sudo chown root:root -R ./g2s-package/*
 sudo chmod 755 ./g2s-package/DEBIAN/postinst
