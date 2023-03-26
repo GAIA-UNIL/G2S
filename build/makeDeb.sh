64e1946f438a6eeade442a440be22bc805582a51
Mathieu Gravey <mathieu@mgravey.com>
2023-03-26 13:52:58 +0200

    update base on GPT-4

--- build/makeDeb.sh
+++ build/makeDeb.sh
@@ -10,6 +10,15 @@ objcopy --strip-debug --strip-unneeded ./intel-build/ds-l ./g2s-package/usr/bin/
 objcopy --strip-debug --strip-unneeded ./intel-build/errorTest  ./g2s-package/usr/bin/g2s_bin/errorTest 
 objcopy --strip-debug --strip-unneeded ./intel-build/auto_qs  ./g2s-package/usr/bin/g2s_bin/auto_qs 
 
+# Read the version number from the version file
+version_number=$(cat ../version)
+
+# Replace the version placeholder in the man page source, in-place
+sed -i -e "s/Version x.y.z/Version ${version_number}/g" ./g2s-package/share/man/man1/g2s.1
+
+gzip -n ./g2s-package/share/doc/g2s/changelog.Debian
+gzip -n ./g2s-package/share/man/man1/g2s.1
+
 
 sudo chown root:root -R ./g2s-package/*
 sudo chmod 755 ./g2s-package/DEBIAN/postinst
@@ -18,5 +27,5 @@ sudo chmod 755 -R ./g2s-package/usr/bin/*
 sudo chmod 755 -R ./g2s-package/lib/systemd/system/*
 sudo chmod 644 ./g2s-package/lib/systemd/system/g2s.service
 sudo chmod 644 ./g2s-package/usr/bin/g2s_bin/algosName.config
-sed -i "8s/.*/& $(cat ../version)/" ./g2s-package/DEBIAN/control 
+sed -i -e "s/Version: x.y.z/Version: ${version_number}/g" ./g2s-package/DEBIAN/control
 dpkg-deb --build ./g2s-package
