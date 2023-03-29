2f03553a53b7db665f270300edf687ba0e7f0ef4
Mathieu Gravey <mathieu@mgravey.com>
2023-03-29 12:52:35 +0200

    Update g2s.rb

--- build/g2s-package/g2s-brew/g2s.rb
+++ build/g2s-package/g2s-brew/g2s.rb
@@ -1,35 +1,35 @@
 class G2s < Formula
   desc "Toolbox for geostatistical simulations."
   homepage "https://gaia-unil.github.io/G2S/"
+  version "x.y.z"
   url "https://github.com/GAIA-UNIL/g2s/archive/COMMIT_HASH.tar.gz"
   sha256 "COMMIT_HASH256"
   license "GPL-3.0-only"
-  version "x.y.z"
 
   # Add dependencies
-  depends_on "zlib"
-  depends_on "fftw"
-  depends_on "zeromq"
-  depends_on "libomp"
-  depends_on "jsoncpp"
   depends_on "cppzmq"
   depends_on "curl"
+  depends_on "fftw"
+  depends_on "jsoncpp"
+  depends_on "libomp"
+  depends_on "zeromq"
+  depends_on "zlib"
 
-   def install
-
+  def install
     cd "build" do
       # Run "make c++ -j"
-      system "make", "c++", "-j", "CXXFLAGS=-Xclang -fopenmp -I#{Formula["fftw"].opt_include}", "LIB_PATH=-L#{Formula["fftw"].opt_lib} -L#{Formula["libomp"].opt_lib} -lomp"
+      system "make", "c++", "-j", "CXXFLAGS=-Xclang -fopenmp -I#{Formula["fftw"].opt_include}", \
+          "LIB_PATH=-L#{Formula["fftw"].opt_lib} -L#{Formula["libomp"].opt_lib} -lomp"
     end
 
     # Copy g2s_server and other from the c++-build folder to the brew bin folder
-    (bin).install "build/g2s-package/g2s-brew/g2s" 
-    (prefix/"g2s_bin").install "build/c++-build/g2s_server" 
-    (prefix/"g2s_bin").install "build/c++-build/echo" 
-    (prefix/"g2s_bin").install "build/c++-build/qs" 
-    (prefix/"g2s_bin").install "build/c++-build/nds" 
-    (prefix/"g2s_bin").install "build/c++-build/ds-l" 
-    (prefix/"g2s_bin").install "build/c++-build/errorTest" 
+    bin.install "build/g2s-package/g2s-brew/g2s"
+    (prefix/"g2s_bin").install "build/c++-build/g2s_server"
+    (prefix/"g2s_bin").install "build/c++-build/echo"
+    (prefix/"g2s_bin").install "build/c++-build/qs"
+    (prefix/"g2s_bin").install "build/c++-build/nds"
+    (prefix/"g2s_bin").install "build/c++-build/ds-l"
+    (prefix/"g2s_bin").install "build/c++-build/errorTest"
     (prefix/"g2s_bin").install "build/c++-build/auto_qs"
     (prefix/"g2s_bin").install "build/algosName.config"
 
@@ -38,37 +38,21 @@ class G2s < Formula
     # fish_completion.install "build/g2s-package/g2s-brew/g2s-completion.fish"
   end
 
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{bin}/g2s</string>
-          <string>server</string>
-          <string>-kod</string>
-          <string>${FLAGS}</string>
-        </array>
-        <key>RunAtLoad</key>
-        <false/>
-        <key>KeepAlive</key>
-        <true/>
-        <key>StandardErrorPath</key>
-        <string>/tmp/#{plist_name}.err</string>
-        <key>StandardOutPath</key>
-        <string>/tmp/#{plist_name}.out</string>
-      </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"g2s", "server", "-kod"]
+    keep_alive true
   end
-
 
   test do
-    # Test if the software is functioning correctly
-    system "#{bin}/g2s", "--help"
+    begin
+      pid = fork do
+        exec bin/"g2s", "server"
+      end
+      sleep 3
+    ensure
+      Process.kill("TERM", pid)
+      Process.wait(pid)
+    end
   end
 end
+
